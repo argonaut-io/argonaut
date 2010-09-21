9d00871d272b2611ba5cdba3e806d9e69c95dd72
Tony Morris <code@tmorris.net>
2010-09-22 09:12:10 +1000

    formatting

--- src/test/scala/com/ephox/argonaut/JsonTest.scala
+++ src/test/scala/com/ephox/argonaut/JsonTest.scala
@@ -10,6 +10,7 @@ object JsonTest extends Properties("Json") {
         (List(j.isNull, j.isBool, j.isNumber, j.isString, j.isArray, j.isObject) filter (z => z) length) == 1)
 
   property("If is a number, then has a number value") =
-      forAll((j: Json) => j.number.isDefined == j.isNumber)
+      forAll((j: Json) =>
+        j.number.isDefined == j.isNumber)
     
 }
