cfcc9f8dee052c629fe2d8be4f7f98e395eba22e
dev <support@ephox.com>
2010-09-21 16:43:17 +1000

    Story: Json Parser [id=17924] Starting to test.

--- src/test/scala/com/ephox/argonaut/ParserTest.scala
+++ src/test/scala/com/ephox/argonaut/ParserTest.scala
@@ -19,10 +19,13 @@ object ParserTest extends Properties("Parser") {
           forAll((s: SometimesNullString) => (s == SometimesNullString("null")) == p(subject.xnull, s.s).successful)
 
   property("null parses to null") =
-          forAll((s: SometimesNullString) => (s == SometimesNullString("null")) ==> (p(subject.xnull, s.s).get == jsonNull))
+          forAll((s: SometimesNullString) => (s == SometimesNullString("null")) ==> p(subject.xnull, s.s).get.isNull)
 
   property("boolean parses ok") =
           forAll((s: SometimesBoolString) => (List("true", "false").contains(s.s) == p(subject.xboolean, s.s).successful))
+
+  property("boolean parses to bool") =
+          forAll((s: SometimesBoolString) => (List("true", "false").contains(s.s) ==> p(subject.xboolean, s.s).get.isBool))
   
   def p(k: subject.Parser[Json], s: String) = {
     val r = new CharSequenceReader(s)
