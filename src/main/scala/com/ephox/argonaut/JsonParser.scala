f77fbe33548050308945de9d71878dc9e97e7370
Mark Hibberd <mth@fire.markh.trinity>
2010-09-21 22:02:31 +1000

    Added some more parsers.

--- src/main/scala/com/ephox/argonaut/JsonParser.scala
+++ src/main/scala/com/ephox/argonaut/JsonParser.scala
@@ -6,30 +6,45 @@ import Json._
 class JsonParser extends Parsers {
   type Elem = Char
 
-  val jsonBoolString: List[Char] => Json = (s: List[Char]) => jsonBool(s == "true".toList)
+  def jnull = acceptSeq("null") ^^^ jsonNull
+  def jboolean = (jfalse ||| jtrue) ^^ jsonBool
+  def jfalse = acceptSeq("false") ^^^ false
+  def jtrue = acceptSeq("true") ^^^ true
 
-  val jnull = acceptSeq("null") ^^^ jsonNull
-
-  val jboolean = oneOf(List("true", "false")) ^^ jsonBoolString
-
-  val e = oneOf(List("e", "e+", "e-", "E", "E+", "E-"))
-
-  val digit1to9 = '1' ||| '2' ||| '3' ||| '4' ||| '5' ||| '6' ||| '7' ||| '8' ||| '9'
-
-  val digit = '0' ||| digit1to9
-
-  val digits = digit +
-
-  val exp = e ~ digits
-
-  val frac = '.' ~ digits
-
-  val int = digit ||| (digit1to9 ~ digits) ||| ('-' ~ digit) ||| ('-' ~ digit1to9 ~ digits)
-
-  val jnumber = int ||| (int ~ frac) ||| (int ~ exp) ||| (int ~ frac ~ exp) ^^ {
+  def number = int ||| (int ~ frac) ||| (int ~ exp) ||| (int ~ frac ~ exp) ^^ {
     case i:List[Elem] => java.lang.Double.valueOf(i.toString)
     case x:Any => error("todo: " + x)
   }
+
+
+//  def jobject: Parser[Json] = acceptSeq("{}") ||| ('{' ~ members ~ '}')
+//  def members = pair ||| (pair ~ ',' ~ members)
+//  def pair = string ~ ':' ~ value
+//  def array: Parser[Json] = acceptSeq("[]") ||| ('[' ~ elements ~ ']')
+//  def elements = value ||| ('[' ~ elements ~ ']')
+  def value = jstring  ||| jboolean ||| jnull // ||| array ||| jobject
+
+  def jstring = string ^^ jsonString
+
+  def string = ('"' ~> optionalChars <~ '"') ^^ { (l: List[Char]) => l.toString}
+
+
+  def chars = char+
+  def optionalChars = char*
+  def char = basicchar ||| escapedchar
+  def basicchar  = elem("char", (c: Char) => c != '"' && c != '\\')
+  def escapedchar  = '\\' ~> escapecode ^^ {(c: Char) => c}
+  def escapecode = '"' ||| '\\' ||| '/' ||| 'b' ||| 'f' ||| 'n' ||| 'r' ||| 't' ||| unicode
+  def unicode: Parser[Char]  = ('u' ~ repN(4, hex)) ^^^ '?' // FIX evaluate....
+  def hex = digit ||| 'a' ||| 'b' ||| 'c' ||| 'd' ||| 'e' ||| 'f'
+
+  def int = digit ||| (digit1to9 ~ digits) ||| ('-' ~ digit) ||| ('-' ~ digit1to9 ~ digits)
+  def frac = '.' ~ digits
+  def exp = e ~ digits
+  def digits = digit+
+  def digit = '0' ||| digit1to9
+  def digit1to9 = '1' ||| '2' ||| '3' ||| '4' ||| '5' ||| '6' ||| '7' ||| '8' ||| '9'
+  def e = oneOf(List("e", "e+", "e-", "E", "E+", "E-"))
 
 
   def oneOf[ES](seqs: Iterable[ES])(implicit e : ES => Iterable[Elem]) = seqs map (acceptSeq[ES] _) reduceRight(_ ||| _)
