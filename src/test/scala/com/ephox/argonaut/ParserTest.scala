191eaf39df3d8093f0977b1b102648674b0a1116
dev <support@ephox.com>
2010-09-21 10:52:39 +1000

    Story: Json Parser [id=17924] Broken test.

--- src/test/scala/com/ephox/argonaut/ParserTest.scala
+++ src/test/scala/com/ephox/argonaut/ParserTest.scala
@@ -1,3 +1,8 @@
 package com.ephox.argonaut
 
- 
+import org.scalacheck.Prop._
+import org.scalacheck.Properties
+
+object ParserTest extends Properties("Parser") {
+  property("Addition commutes") = forAll((x: Int, y: Int) => x - y == y + x)
+}
