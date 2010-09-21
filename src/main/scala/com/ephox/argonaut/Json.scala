4458b45205aa05055c379a31038a54912f41e1f6
Tony Morris <code@tmorris.net>
2010-09-21 16:57:02 +1000

    documentation

--- src/main/scala/com/ephox/argonaut/Json.scala
+++ src/main/scala/com/ephox/argonaut/Json.scala
@@ -106,66 +106,134 @@ sealed trait Json {
          _ => f,
          t(_))
 
+  /**
+   * Return `true` if this JSON value is `null`, otherwise, `false`.
+   */
   def isNull =
     ifNull(true, false)
 
+  /**
+   * Return `true` if this JSON value is a boolean, otherwise, `false`.
+   */
   def isBool =
     ifBool(_ => true, false)
 
+  /**
+   * Return `true` if this JSON value is a number, otherwise, `false`.
+   */
   def isNumber =
     ifNumber(_ => true, false)
 
+  /**
+   * Return `true` if this JSON value is a string, otherwise, `false`.
+   */
   def isString =
     ifString(_ => true, false)
 
+  /**
+   * Return `true` if this JSON value is an array, otherwise, `false`.
+   */
   def isArray =
     ifArray(_ => true, false)
 
+  /**
+   * Return `true` if this JSON value is an object, otherwise, `false`.
+   */
   def isObject =
     ifObject(_ => true, false)
 
+  /**
+   * Return `true` if this JSON value is a boolean with a `true` value, otherwise, `false`.
+   */
   def ifBoolTrue[X](t: => X, f: => X) =
     ifBool(x => if(x) t else f, f)
 
+  /**
+   * Return `true` if this JSON value is a boolean with a `false` value, otherwise, `false`.
+   */
   def ifBoolFalse[X](t: => X, f: => X) =
     ifBool(x => if(x) f else t, f)
 
+  /**
+   * Returns the possible number of this JSON value.
+   */
   def number =
     ifNumber(Some(_), None)
 
+  /**
+   * Returns the number of this JSON value, or the given default if this JSON value is not a number.
+   *
+   * @param The default number if this JSON value is not a number.
+   */
   def numberOr(d: => JsonNumber) =
     number getOrElse d
 
   def numberOrZero =
     numberOr(0D)
 
+  /**
+   * Returns the possible string of this JSON value.
+   */
   def string =
     ifString(Some(_), None)
 
+  /**
+   * Returns the string of this JSON value, or the given default if this JSON value is not a string.
+   *
+   * @param The default string if this JSON value is not a string.
+   */
   def stringOr(s: => String) =
     string getOrElse s
 
+  /**
+   * Returns the string of this JSON value, or an empty string if this JSON value is not a string.
+   */
   def stringOrEmpty =
     stringOr("")
 
+  /**
+   * Returns the possible array of this JSON value.
+   */
   def array =
     ifArray(Some(_), None)
 
+  /**
+   * Returns the array of this JSON value, or the given default if this JSON value is not an array.
+   *
+   * @param The default array if this JSON value is not an array.
+   */
   def arrayOr(a: => JsonArray) =
     array getOrElse a
 
+  /**
+   * Returns the array of this JSON value, or an empty array if this JSON value is not an array.
+   */
   def arrayOrEmpty =
     arrayOr(Nil)
 
+  /**
+   * Returns the possible object of this JSON value.
+   */
   def objectt =
     ifObject(Some(_), None)
 
+  /**
+   * Returns the object of this JSON value, or the given default if this JSON value is not a object.
+   *
+   * @param The default object if this JSON value is not an object.
+   */
   def objectOr(o: => JsonObject) =
     objectt getOrElse o
 
+  /**
+   * Returns the object of this JSON value, or the empty object if this JSON value is not an object.
+   */
   def objectOrEmpty =
     objectOr(Nil)
   
+  /**
+   * Returns the possible object map of this JSON value.
+   */
   lazy val objectMap: Option[JsonObjectMap] =
     fold(None,
          _ => None,
@@ -174,9 +242,17 @@ sealed trait Json {
          _ => None,
          x => Some(x.toMap))
 
+  /**
+   * Returns the object map of this JSON value, or the given default if this JSON value is not an object.
+   *
+   * @param The default object map if this JSON value is not an object.
+   */
   def objectMapOr(m: => JsonObjectMap) =
     objectMap getOrElse m
 
+  /**
+   * Returns the object map of this JSON value, or the empty map if this JSON value is not an object.
+   */
   def objectMapOrEmpty =
     objectMapOr(Map.empty)
 
