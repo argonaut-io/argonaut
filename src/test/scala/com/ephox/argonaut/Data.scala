f6594817f83577388cbc26cb4a3cef6adb7ae4da
Tony Morris <code@tmorris.net>
2010-09-22 08:44:49 +1000

    tidy up

--- src/test/scala/com/ephox/argonaut/Data.scala
+++ src/test/scala/com/ephox/argonaut/Data.scala
@@ -1,22 +1,22 @@
 package com.ephox.argonaut
 
 import org.scalacheck.Arbitrary.arbitrary
-import org.scalacheck.Gen.{frequency, choose, listOfN}
+import org.scalacheck.Gen.{frequency, choose, listOfN, value}
 import Json._
 import org.scalacheck.{Gen, Arbitrary}
 
 object Data {
   implicit def ArbitraryJson: Arbitrary[Json] = {
-    val n = arbitrary[Unit] map (_ => jsonNull)
+    val n = value(jsonNull)
     val b = arbitrary[Boolean] map (jsonBool(_))
     val m = arbitrary[JsonNumber] map (jsonNumber(_))
     val s = arbitrary[String] map (jsonString(_))
-    val a =  for(n <- choose(0, 10);
-                 j <- listOfN(n, arbitrary[Json]))
-             yield jsonArray(j)
+    val a = for(n <- choose(0, 10);
+                j <- listOfN(n, arbitrary[Json]))
+            yield jsonArray(j)
     val o = for(n <- choose(0, 10);
-                 j <- listOfN(n, arbitrary[(String, Json)]))
-             yield jsonObject(j)
+                j <- listOfN(n, arbitrary[(String, Json)]))
+            yield jsonObject(j)
 
     Arbitrary(frequency((10, n), (10, b), (10, m), (10, s), (1, a), (1, o)))
   }
