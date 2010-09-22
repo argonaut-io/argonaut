4a704c8be11e3ac74c33c1e3388cc09ab7bfc24b
Tony Morris <code@tmorris.net>
2010-09-22 10:14:48 +1000

    formatting

--- src/test/scala/com/ephox/argonaut/JsonTest.scala
+++ src/test/scala/com/ephox/argonaut/JsonTest.scala
@@ -12,5 +12,16 @@ object JsonTest extends Properties("Json") {
   property("If is a number, then has a number value") =
       forAll((j: Json) =>
         j.number.isDefined == j.isNumber)
-    
+
+  property("If is a string, then has a string value") =
+      forAll((j: Json) =>
+        j.string.isDefined == j.isString)
+
+  property("If is an array, then has an array value") =
+      forAll((j: Json) =>
+        j.array.isDefined == j.isArray)
+
+  property("If is an object, then has an object value") =
+      forAll((j: Json) =>
+        j.objectt.isDefined == j.isObject)
 }
