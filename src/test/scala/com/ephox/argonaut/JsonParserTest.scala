71bc6855505a98e37fb3ba268cfe7e6419789b16
Mark Hibberd <mth@fire.markh.trinity>
2010-09-23 12:26:11 +1000

    Small things.

--- src/test/scala/com/ephox/argonaut/JsonParserTest.scala
+++ src/test/scala/com/ephox/argonaut/JsonParserTest.scala
@@ -9,18 +9,22 @@ object JsonParserTest extends Properties("Parser") {
   val subject = new JsonParser
 
   property("null parses ok") =
-          forAll((s: SometimesNullString) => (s == SometimesNullString("null")) == p(subject.jnull, s.s).successful)
+          forAll((s: SometimesNullString) =>
+            (s == SometimesNullString("null")) == p(subject.jnull, s.s).successful)
 
   property("null parses to null") =
-          forAll((s: SometimesNullString) => s != SometimesNullString("null") || p(subject.jnull, s.s).get.isNull)
+          forAll((s: SometimesNullString) =>
+            s != SometimesNullString("null") || p(subject.jnull, s.s).get.isNull)
 
   property("boolean parses ok") =
-          forAll((s: SometimesBoolString) => (List("true", "false").contains(s.s) == p(subject.jboolean, s.s).successful))
+          forAll((s: SometimesBoolString) =>
+            (List("true", "false").contains(s.s) == p(subject.jboolean, s.s).successful))
 
   property("boolean parses to bool") =
-          forAll((s: SometimesBoolString) => (List("true", "false").contains(s.s) ==> p(subject.jboolean, s.s).get.isBool))
+          forAll((s: SometimesBoolString) =>
+            (List("true", "false").contains(s.s) ==> p(subject.jboolean, s.s).get.isBool))
 
-  // FIX escaped chars? don't think they are being generated at the moment
+  // FIX unicode escaped chars? don't think they are being generated at the moment
   // FIX break this property apart, testing a few things at the moment.   
   property("all that encodes can be decoded") =
           forAll({(j: Json) =>
@@ -28,8 +32,6 @@ object JsonParserTest extends Properties("Parser") {
               val compact = p(subject.jvalue, JsonPrinter.compact(j))
               pretty.successful && pretty.get == j && compact.successful && compact.get == j
             })
-
-  // FIX flush out other properties.
 
   def p(k: subject.Parser[Json], s: String) =
     k(new CharSequenceReader(s))
