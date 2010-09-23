2c02fbe636de41551e94c82c88f7cfe63f2901e8
Mark Hibberd <mth@fire.markh.trinity>
2010-09-23 12:21:16 +1000

    Attempt at cleaning up pretty printer.

--- src/main/scala/com/ephox/argonaut/JsonPrinter.scala
+++ src/main/scala/com/ephox/argonaut/JsonPrinter.scala
@@ -5,37 +5,45 @@ object JsonPrinter extends Application{
 
   def pretty(json: Json): String = prettyPrint(json, "    ", "\n",  "")
 
-  // FIX Naive test to get some testing happening... delete. Clean up.  
+  // FIX simplify this fugly duckling
   def prettyPrint(json: Json, tab:String, delimbreak: String, currentindent: String): String = {
 
     def join(s: List[String]) = s.mkString("," + delimbreak + currentindent + tab)
 
     def recurse(json: Json) = prettyPrint(json, tab, delimbreak, currentindent + tab)
 
+    def bracket(s: String, open: String, close: String) =
+        open + delimbreak + currentindent + tab + s +  delimbreak + currentindent + close
+
+    def entries[A](entries: List[A], open: String, close: String, f: A => String) =
+        bracket(join(entries map f), open, close)
+
+    def printString(s: String) =  '"' + escape(s) + '"'
+
+    def escape(s: String) =
+      s.replace("\\", "\\\\").
+        replace("\"", "\\\"").
+        replace("\b", "\\b").
+        replace("\f", "\\f").
+        replace("\n", "\\n").
+        replace("\r", "\\r").
+        replace("\t", "\\t")
+
     json.fold (
       "null",
       _.toString,
       _.toString,
       printString,
-      xs => "[" + delimbreak + currentindent + tab +
-          join(xs.map(recurse(_))) +
-       delimbreak + currentindent + "]",
-      xs => "{" + delimbreak + currentindent + tab +
-          join(xs.map({case (k, v) => printString(k) + ": " + recurse(v)})) +
-       delimbreak + currentindent + "}"
+      xs =>
+        entries(xs, "[", "]", recurse(_:Json)),
+      xs =>
+        // FIX why does the inferencer fail here, but not for `map`.
+        entries(xs, "{", "}", {case (k, v) => printString(k) + ": " + recurse(v)}:Function[(String, Json), String])
     )
   }
 
-  def printString(s: String) =  '"' + escape(s) + '"'
 
 
 
-  def escape(s: String) =
-    s.replace("\\", "\\\\").
-      replace("\"", "\\\"").
-      replace("\b", "\\b").
-      replace("\f", "\\f").
-      replace("\n", "\\n").
-      replace("\r", "\\r").
-      replace("\t", "\\t")
+
 }
