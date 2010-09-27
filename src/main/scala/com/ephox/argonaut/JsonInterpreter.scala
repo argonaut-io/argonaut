1b5b0ce9f557f4778ac5d50e5c14f03f35733a8c
dev <support@ephox.com>
2010-09-27 12:17:29 +1000

    Story: Json Parser [id=17924] Fun with implicit conversions

--- src/main/scala/com/ephox/argonaut/JsonInterpreter.scala
+++ src/main/scala/com/ephox/argonaut/JsonInterpreter.scala
@@ -8,7 +8,6 @@ object JsonInterpreter {
 
   // FIX 17924 27/09/2010 get these to return Option[T] or some Result[T]... or some other technique to make them fail without exploding
 
-
   implicit def mk[T](f: Json => T) = new Interpret[T] {
     override def apply(j: Json): T = f(j)
   }
@@ -20,9 +19,7 @@ object JsonInterpreter {
     toJavaList(j.array.get.map(sub.apply(_)))
 
   val string: Interpret[String] = (j: Json) => j.string.get
-
   val number: Interpret[Double] = (j: Json) => j.number.get
-
 
   val scalaListString = scalaList(string)
   val javaListString = javaList(string)
