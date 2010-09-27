84eb34e021c063dc7c6d92e946ef44efef6e80bf
dev <support@ephox.com>
2010-09-27 12:16:59 +1000

    Story: Json Parser [id=17924] Fun with implicit conversions

--- src/main/scala/com/ephox/argonaut/JsonInterpreter.scala
+++ src/main/scala/com/ephox/argonaut/JsonInterpreter.scala
@@ -6,23 +6,23 @@ import j.Interpret
 
 object JsonInterpreter {
 
-  // FIX 17924 27/09/2010 get these to return Option[T]
+  // FIX 17924 27/09/2010 get these to return Option[T] or some Result[T]... or some other technique to make them fail without exploding
 
-  def scalaList[T](subbie: Interpret[T]): Interpret[scala.List[T]] = new Interpret[scala.List[T]] {
-    override def apply(j: Json): scala.List[T] = j.array.get.map(subbie.apply(_))
+
+  implicit def mk[T](f: Json => T) = new Interpret[T] {
+    override def apply(j: Json): T = f(j)
   }
 
-  def javaList[T](subbie: Interpret[T]): Interpret[java.util.List[T]] = new Interpret[java.util.List[T]] {
-    override def apply(j: Json): java.util.List[T] = toJavaList(j.array.get.map(subbie.apply(_)))
-  }
+  def scalaList[T](sub: Interpret[T]): Interpret[scala.List[T]] = (j: Json) =>
+    j.array.get.map(sub.apply(_))
 
-  val string: Interpret[String] = new Interpret[String] {
-    override def apply(j: Json): String = j.string.get
-  }
+  def javaList[T](sub: Interpret[T]): Interpret[java.util.List[T]] = (j: Json) =>
+    toJavaList(j.array.get.map(sub.apply(_)))
 
-  val number: Interpret[Double] = new Interpret[Double] {
-    override def apply(j: Json): Double = j.number.get
-  }
+  val string: Interpret[String] = (j: Json) => j.string.get
+
+  val number: Interpret[Double] = (j: Json) => j.number.get
+
 
   val scalaListString = scalaList(string)
   val javaListString = javaList(string)
