7f47a07184496053e248b0850242bc61bde72899
void <support@ephox.com>
2011-06-30 12:54:51 +1000

    FromJson things

--- src/main/scala/com/ephox/argonaut/JsonQuery.scala
+++ src/main/scala/com/ephox/argonaut/JsonQuery.scala
@@ -5,16 +5,22 @@ import Json._
 import JsonValue._
 
 trait JsonQuery {
-  def value[A](json: Json, path: String*)(implicit from: FromJson[A]) = for {
+  val json: Json
+
+  def option[A](path: String*)(implicit from: FromJson[A]) = for {
+    j <- find(json, path.toList).map[Option[Json]](v => Some(v)).flatMapError(_ => jsonValue(None))
+    r <- j.traverse(x => from.apply(x))
+  } yield r
+
+  def value[A](path: String*)(implicit from: FromJson[A]) = for {
     j <- find(json, path.toList)
     r <- j.to[A]
   } yield r
 
-  def list[A](json: Json, path: String*)(implicit from: FromJson[A]): JsonValue[List[A]] = for {
+  def list[A](path: String*)(implicit from: FromJson[A]): JsonValue[List[A]] = for {
     j <- find(json, path.toList)
     r <- j.to[List[A]]
   } yield r
-
 
   def find(json: Json, path: List[String]): JsonValue[Json] =
     (json -|| path).json(
@@ -26,3 +32,11 @@ trait JsonQuery {
     jsonError[A]("Path [" + path.mkString("/") + "] " + note + ", in json [\n" + JsonPrinter.pretty(json)+ "\n]")
 }
 
+
+object JsonQuery extends JsonQuerys
+
+trait JsonQuerys {
+  implicit def JsonToJsonQuery(j: Json): JsonQuery = new JsonQuery {
+    val json = j
+  }
+}
