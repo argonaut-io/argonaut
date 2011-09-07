f3770710cbac165ddf8c7e1e8b4a72b908bed5aa
void <support@ephox.com>
2011-09-07 14:56:59 +1000

    deps

--- project/build/Argonaut.scala
+++ project/build/Argonaut.scala
@@ -3,9 +3,9 @@ import sbt._
 class Argonaut(info: ProjectInfo) extends DefaultProject(info) {
   val scalaToolsSnapshots = "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots/"
 
-  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.8" withSources
+  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.9" withSources
 
-  val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.1" withSources
+  val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.3" withSources
 
   override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 }
