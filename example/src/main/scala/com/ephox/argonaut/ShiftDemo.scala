220af802d39371ea0e01ab99cc0cc45633615fcf
Tony Morris <tmorris@tmorris.net>
2012-06-21 10:38:25 +1000

    comment demo?

--- example/src/main/scala/com/ephox/argonaut/ShiftDemo.scala
+++ example/src/main/scala/com/ephox/argonaut/ShiftDemo.scala
@@ -39,11 +39,17 @@ object ShiftDemo {
         }
       """
 
+    // if has field "wiggle", go to field "xyz"
     val r1 = shift.when(_ hasField "wiggle").downField("xyz")
+    // down to field "values", into array, into array, right on array, reverse string at focus, up, right on array
     val r2 = shift.downField("values").downArray.downArray.right.withFocus(jStringL =>= (_.reverse)).up.right
+    // try r1, otherwise r2, then set focus to string "cat"
     val r = (r1 ||| r2) := jString("cat")
-    val s = r.up.downArray.downArray <| j.pparse
+    // parse JSON string and apply cursor-shift operations
+    val s = r <| j.pparse
+    // run cursor-shift and pretty-print with two-space indent
     s.cursor map (c => (-c).spaces2) foreach println
+    // print new JSON value and history
     s.println
   }
 }
