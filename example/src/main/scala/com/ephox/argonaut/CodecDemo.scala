3b1cd2d26b83c7a55a6316023761b7e81f91b0fa
Tony Morris <tmorris@tmorris.net>
2012-06-29 01:38:17 +1000

    fix codec demo

--- example/src/main/scala/com/ephox/argonaut/CodecDemo.scala
+++ example/src/main/scala/com/ephox/argonaut/CodecDemo.scala
@@ -11,10 +11,10 @@ object CodecDemo {
       DecodeJson(j => j.array match {
         case Some(n::a::s::c::Nil) =>
           for {
-            nn <- implicitly[DecodeJson[String]].apply(n)
-            aa <- implicitly[DecodeJson[Int]].apply(a)
-            ss <- implicitly[DecodeJson[Option[Person]]].apply(s)
-            cc <- implicitly[DecodeJson[List[Person]]].apply(c)
+            nn <- n.decode[String]
+            aa <- a.decode[Int]
+            ss <- s.decode[Option[Person]]
+            cc <- c.decode[List[Person]]
           } yield Person(nn, aa, ss, cc)
         case _ => decodeError(j, "Person")
       })
@@ -22,10 +22,10 @@ object CodecDemo {
     implicit val EncodePerson: EncodeJson[Person] =
       EncodeJson({
         case Person(n, a, s, c) => jArray(List(
-          implicitly[EncodeJson[String]].apply(n)
-        , implicitly[EncodeJson[Int]].apply(a)
-        , implicitly[EncodeJson[Option[Person]]].apply(s)
-        , implicitly[EncodeJson[List[Person]]].apply(c)
+          n.encode
+        , a.encode
+        , s.encode
+        , c.encode
         ))
       }, "Person")
   }
@@ -33,9 +33,9 @@ object CodecDemo {
   def main(args: Array[String]) {
     val children = List(Person("Bob", 10, None, Nil), Person("Jill", 12, None, Nil))
     val fred = Person("Fred", 40, Some(Person("Mary", 41, None, children)), Person("Tom", 15, None, Nil) :: children)
-    val encode = implicitly[EncodeJson[Person]].apply(fred)
-    println(encode.spaces2)
-    val decode = implicitly[DecodeJson[Person]].apply(encode)
+    val enc = fred.encode
+    println(enc.spaces2)
+    val decode = enc.decode[Person]
     println(decode.run match {
       case Left(e) => e
       case Right(p) => p.toString
