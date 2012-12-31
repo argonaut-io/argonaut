8f580fff2b6703b342fef1891b25efd05a22bf9f
markhibberd <mark@hibberd.id.au>
2012-12-31 20:04:23 +1000

    Remove usages of setName from examples.
    Fixes #15.

--- src/test/scala/argonaut/example/CodecExample.scala
+++ src/test/scala/argonaut/example/CodecExample.scala
@@ -19,19 +19,19 @@ object CodecExample extends Specification {
   def is = "CodecExample" ^
     "Array codec" ! {
       implicit val DecodePerson: DecodeJson[Person] =
-        jdecode2(Person(_: String, _: Int)) setName "Person"
+        jdecode2(Person(_: String, _: Int))
 
       implicit val EncodePerson: EncodeJson[Person] =
-        jencode2((p: Person) => (p.name, p.age)) setName "Person"
-      
+        jencode2((p: Person) => (p.name, p.age))
+
       encodeDecode("""["Fred","40"]""")
-    } ^ 
+    } ^
     "Object codec" ! {
       implicit val DecodePerson: DecodeJson[Person] =
-        jdecode2L(Person(_: String, _: Int))("name", "age") setName "Person"
+        jdecode2L(Person(_: String, _: Int))("name", "age")
 
       implicit val EncodePerson: EncodeJson[Person] =
-        jencode2L((p: Person) => (p.name, p.age))("name", "age") setName "Person"
+        jencode2L((p: Person) => (p.name, p.age))("name", "age")
 
       encodeDecode("""{"name":"Fred","age":"40"}""")
     }
