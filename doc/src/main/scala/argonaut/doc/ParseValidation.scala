75b5f3812b74880450e2a6690ce72d8018e84545
markhibberd <mark@hibberd.id.au>
2013-01-02 22:37:50 +1000

    Clean up ParseValidation documentation.

--- doc/src/main/scala/argonaut/doc/ParseValidation.scala
+++ doc/src/main/scala/argonaut/doc/ParseValidation.scala
@@ -3,16 +3,12 @@ package argonaut.doc
 import argonaut._, Argonaut._
 
 object ParseValidation extends App {
-
-  val json = """
+  val result = Parse.parse("""
     {
       "name" : "bob",
       "age" : 49
     }
-  """
-
-  val result = Parse.parse(json)
+  """)
 
   println(if (result.isSuccess) "Parse was successfull" else "Parse failed")
-
 }
