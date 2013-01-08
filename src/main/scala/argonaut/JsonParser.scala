7d69ab464cf51b0a24a05b103649f9f4be67205c
sean <git@futurenotfound.com>
2013-01-08 01:14:13 +0000

    Created WrappedCharArray type within JsonParser as an alternative to String because we heavily substring when parsing and in Java 7u6 and upwards the performance of that has been destroyed.

--- src/main/scala/argonaut/JsonParser.scala
+++ src/main/scala/argonaut/JsonParser.scala
@@ -6,11 +6,18 @@ import Scalaz._
 import Json._
 import java.lang.StringBuilder
 import scala.collection.mutable.Builder
+import util.MurmurHash
 
 object JsonParser {
-  sealed trait TokenStream
-  sealed case class TokenStreamElement(element: JSONToken, next: () => TokenStream) extends TokenStream
-  case object TokenStreamEnd extends TokenStream
+  sealed trait TokenStream {
+    def toStream: Stream[JSONToken]
+  }
+  sealed case class TokenStreamElement(element: JSONToken, next: () => TokenStream) extends TokenStream {
+    def toStream: Stream[JSONToken] = Stream.cons(element, next().toStream)
+  }
+  case object TokenStreamEnd extends TokenStream {
+    def toStream: Stream[JSONToken] = Stream.empty
+  }
 
   sealed abstract class JSONToken {
     def originalStringContent: String
@@ -60,22 +67,95 @@ object JsonParser {
     final def appendToBuilder(builder: StringBuilder) = builder.append(parsedStringContent)
   }
   object EscapedCharacterToken {
-    val charMap: Map[String, EscapedCharacterToken] = Map(
-      "\\r" -> EscapedCharacterToken("\\r", "\r"),
-      "\\n" -> EscapedCharacterToken("\\n", "\n"),
-      "\\t" -> EscapedCharacterToken("\\t", "\t"),
-      "\\b" -> EscapedCharacterToken("\\b", "\b"),
-      "\\f" -> EscapedCharacterToken("\\f", "\f"),
-      """\\""" -> EscapedCharacterToken("""\\""", """\"""),
-      """\/""" -> EscapedCharacterToken("""\/""", """/"""),
-      "\\\"" -> EscapedCharacterToken("\\\"", "\"")
+    val charMap: Map[WrappedCharArray, EscapedCharacterToken] = Map(
+      WrappedCharArray.fromString("\\r") -> EscapedCharacterToken("\\r", "\r"),
+      WrappedCharArray.fromString("\\n") -> EscapedCharacterToken("\\n", "\n"),
+      WrappedCharArray.fromString("\\t") -> EscapedCharacterToken("\\t", "\t"),
+      WrappedCharArray.fromString("\\b") -> EscapedCharacterToken("\\b", "\b"),
+      WrappedCharArray.fromString("\\f") -> EscapedCharacterToken("\\f", "\f"),
+      WrappedCharArray.fromString("""\\""") -> EscapedCharacterToken("""\\""", """\"""),
+      WrappedCharArray.fromString("""\/""") -> EscapedCharacterToken("""\/""", """/"""),
+      WrappedCharArray.fromString( "\\\"") -> EscapedCharacterToken("\\\"", "\"")
     ) 
   }
 
-  sealed case class NormalStringToken(originalStringContent: String) extends StringPartToken {
-    final def appendToBuilder(builder: StringBuilder) = builder.append(originalStringContent)
+  sealed case class NormalStringToken(tokenSource: TokenSource) extends StringPartToken {
+    final def originalStringContent = tokenSource.toString
+    final def appendToBuilder(builder: StringBuilder) = tokenSource.appendTo(builder)
   }
   sealed case class UnexpectedContentToken(originalStringContent: String) extends JSONToken
+
+  /**
+   * Alternative to String implemented similarly to how String was prior to Java 7u6.
+   */
+  trait WrappedCharArray {
+    lazy val isEmpty: Boolean = headOption.isEmpty
+    val headOption: Option[Char]
+    def span(p: Char => Boolean): (WrappedCharArray, WrappedCharArray)
+    def take(n: Int): WrappedCharArray
+    def drop(n: Int): WrappedCharArray
+    def startsWith(possibleStart: Array[Char]): Boolean
+    val tail: WrappedCharArray
+    def appendTo(builder: StringBuilder): StringBuilder
+  }
+
+  object WrappedCharArray {
+    private[this] case object EmptyWrappedCharArray extends WrappedCharArray {
+      def span(p: (Char) => Boolean) = (this, this)
+      val tail = this
+      val headOption = None
+      def take(n: Int): WrappedCharArray = this
+      def drop(n: Int): WrappedCharArray = this
+      def startsWith(possibleStart: Array[Char]): Boolean = false
+      def appendTo(builder: StringBuilder): StringBuilder = builder
+      override def toString(): String = ""
+    }
+
+    private[this] def boundedCharArray(chars: Array[Char], start: Int, length: Int): WrappedCharArray = {
+      if (start >= chars.length) EmptyWrappedCharArray
+      else {
+        val safeLength: Int = if (length + start >= chars.length) chars.length - start else length
+        new WrappedCharArray {
+          def span(p: (Char) => Boolean) = {
+            val index = chars.indexWhere(char => !p(char), start)
+            if (index < 0) (this, EmptyWrappedCharArray)
+            else {
+              val newLength = index - start
+              (boundedCharArray(chars, start, newLength), boundedCharArray(chars, index, safeLength - newLength))
+            }
+          }
+          lazy val tail = boundedCharArray(chars, start + 1, safeLength - 1)
+          lazy val headOption = chars(start).some
+          def take(n: Int): WrappedCharArray = boundedCharArray(chars, start, n)
+          def drop(n: Int): WrappedCharArray = boundedCharArray(chars, start + n, safeLength)
+          def startsWith(possibleStart: Array[Char]): Boolean = chars.startsWith(possibleStart, start)
+          def appendTo(builder: StringBuilder): StringBuilder = builder.append(chars, start, safeLength)
+          private[this] final lazy val storedString: String = new java.lang.StringBuilder().append(chars, start, safeLength).toString
+          override def toString(): String = storedString
+          private[this] final lazy val storedHash: Int = {
+            var working: Int = 37
+            for (i <- start to start + safeLength - 1) {
+              val value = chars(i)
+              working = working * 17 + ((value ^ (value >> 32)))
+            }
+            working
+          }
+          override def hashCode() = storedHash
+          override def equals(other: Any): Boolean = other match {
+            case otherWrapped: WrappedCharArray => this.toString() == otherWrapped.toString
+            case _ => false
+          }
+        }
+      }
+    }
+
+    final def fromString(string: String): WrappedCharArray = {
+      val chars = string.toCharArray
+      boundedCharArray(chars, 0, chars.length)
+    }
+  }
+
+  private[this] type TokenSource = WrappedCharArray
   
   private[this] def excerpt(string: String, limit: Int = 50): String = {
     if (string.size > limit) {
@@ -116,14 +196,6 @@ object JsonParser {
   
   private[this] final def expectStringBounds(stream: TokenStream) = expectedSpacerToken(stream, StringBoundsToken, "Expected string bounds")
 
-  private[this] final def expectArrayOpen(stream: TokenStream) = expectedSpacerToken(stream, ArrayOpenToken, "Expected array open token")
-
-  private[this] final def expectArrayClose(stream: TokenStream) = expectedSpacerToken(stream, ArrayCloseToken, "Expected array close token")
-
-  private[this] final def expectObjectOpen(stream: TokenStream) = expectedSpacerToken(stream, ObjectOpenToken, "Expected object open token")
-
-  private[this] final def expectObjectClose(stream: TokenStream) = expectedSpacerToken(stream, ObjectCloseToken, "Expected object close token")
-
   private[this] final def expectEntrySeparator(stream: TokenStream) = expectedSpacerToken(stream, EntrySeparatorToken, "Expected entry separator token")
 
   private[this] final def expectFieldSeparator(stream: TokenStream) = expectedSpacerToken(stream, FieldSeparatorToken, "Expected field separator token")
@@ -137,8 +209,8 @@ object JsonParser {
         val next = for {
           afterEntrySeparator <- if (first) stream.right[String] else expectEntrySeparator(stream)
           streamAndKey <- expectString(afterEntrySeparator)
-          afterFieldSeperator <- expectFieldSeparator(streamAndKey._1)
-          streamAndValue <- expectValue(afterFieldSeperator)
+          afterFieldSeparator <- expectFieldSeparator(streamAndKey._1)
+          streamAndValue <- expectValue(afterFieldSeparator)
         } yield (streamAndValue._1, fields += ((streamAndKey._2.s, streamAndValue._2)))
         next match {
           case \/-((newStream, newFields)) => expectObject(newStream, false, newFields)
@@ -176,6 +248,7 @@ object JsonParser {
       case TokenStreamElement(NullToken, tail) => (tail(), JNull).right
       case TokenStreamElement(NumberToken(numberText), tail) => {
         numberText
+          .mkString
           .parseDouble
           .fold(nfe => "Value [%s] cannot be parsed into a number.".format(numberText).left,
                 doubleValue => (tail(), JNumber(JsonNumber(doubleValue))).right)
@@ -211,14 +284,14 @@ object JsonParser {
     } yield (afterClose, JString(elements._2.result.foldLeft(new StringBuilder())(appendStringPartToBuilder).toString))
   }
 
-  private[this] final def unexpectedContent(json: String): TokenStream = TokenStreamElement(UnexpectedContentToken(json.take(10)), () => TokenStreamEnd)
+  private[this] final def unexpectedContent(json: TokenSource): TokenStream = TokenStreamElement(UnexpectedContentToken(json.take(10).toString), () => TokenStreamEnd)
 
   private[this] final val isNumberChar = (char: Char) => (char >= '0' && char <= '9') || char == '+' || char == '-' || char == 'e' || char == 'E' || char == '.'
 
-  private[this] final def parseNumber(json: String): Option[(NumberToken, String)] = {
+  private[this] final def parseNumber(json: TokenSource): Option[(NumberToken, TokenSource)] = {
     val (possibleNumber, remainder) = json.span(isNumberChar)
     if (possibleNumber.isEmpty) None
-    else (NumberToken(possibleNumber), remainder).some
+    else (NumberToken(possibleNumber.toString), remainder).some
   }
 
   private[this] final val isUnicodeSequenceChar = (char: Char) => {
@@ -229,13 +302,20 @@ object JsonParser {
     char != '"' && char != '\\' && !Character.isISOControl(char)
   }
 
-  private[this] final def tokenizeString(json: String): TokenStream = {
+  private[this] final val speechMark: Array[Char] = Array('"')
+  private[this] final val backslash: Array[Char] = Array('\\')
+  private[this] final val backslashU: Array[Char] = Array('\\', 'u')
+  private[this] final val trueArray: Array[Char] = "true".toArray
+  private[this] final val falseArray: Array[Char] = "false".toArray
+  private[this] final val nullArray: Array[Char] = "null".toArray
+
+  private[this] final def tokenizeString(json: TokenSource): TokenStream = {
     if (json.isEmpty) TokenStreamEnd
-    else if (json.startsWith(""""""")) TokenStreamElement(StringBoundsToken, () => tokenize(json.tail))
-    else if (json.startsWith("""\""")) {
-      if (json.startsWith("\\u")) {
-        val possibleUnicodeSequence = json.drop(2).take(4)
-        if (possibleUnicodeSequence.forall(isUnicodeSequenceChar)) {
+    else if (json.startsWith(speechMark)) TokenStreamElement(StringBoundsToken, () => tokenize(json.tail))
+    else if (json.startsWith(backslash)) {
+      if (json.startsWith(backslashU)) {
+        val possibleUnicodeSequence = json.drop(2).take(4).toString
+        if (possibleUnicodeSequence.length == 4 && possibleUnicodeSequence.forall(isUnicodeSequenceChar)) {
           val unicodeCharToken = UnicodeCharacterToken(possibleUnicodeSequence)
           TokenStreamElement(unicodeCharToken, () => tokenizeString(json.drop(6)))
         } else unexpectedContent(json)
@@ -246,7 +326,7 @@ object JsonParser {
         }
       }
     } else {
-      val (prefix: String, suffix: String) = json.span(isNormalChar)
+      val (prefix, suffix) = json.span(isNormalChar)
       val normalStringToken = NormalStringToken(prefix)
       suffix.headOption match {
         case Some('\"') | Some('\\') => TokenStreamElement(normalStringToken, () => tokenizeString(suffix))
@@ -255,8 +335,10 @@ object JsonParser {
       }
     }
   }
-  
-  final def tokenize(json: String): TokenStream = {
+
+  final def tokenize(json: String): TokenStream = tokenize(WrappedCharArray.fromString(json))
+
+  final def tokenize(json: TokenSource): TokenStream = {
     json.headOption match {
       case Some('[') => TokenStreamElement(ArrayOpenToken, () => tokenize(json.tail))
       case Some(']') => TokenStreamElement(ArrayCloseToken, () => tokenize(json.tail))
@@ -265,9 +347,9 @@ object JsonParser {
       case Some(':') => TokenStreamElement(FieldSeparatorToken, () => tokenize(json.tail))
       case Some(',') => TokenStreamElement(EntrySeparatorToken, () => tokenize(json.tail))
       case Some('"') => TokenStreamElement(StringBoundsToken, () => tokenizeString(json.tail))
-      case Some('t') if json.startsWith("true") => TokenStreamElement(BooleanTrueToken, () => tokenize(json.drop(4)))
-      case Some('f') if json.startsWith("false") => TokenStreamElement(BooleanFalseToken, () => tokenize(json.drop(5)))
-      case Some('n') if json.startsWith("null") => TokenStreamElement(NullToken, () => tokenize(json.drop(4)))
+      case Some('t') if json.startsWith(trueArray) => TokenStreamElement(BooleanTrueToken, () => tokenize(json.drop(4)))
+      case Some('f') if json.startsWith(falseArray) => TokenStreamElement(BooleanFalseToken, () => tokenize(json.drop(5)))
+      case Some('n') if json.startsWith(nullArray) => TokenStreamElement(NullToken, () => tokenize(json.drop(4)))
       case Some(' ') => tokenize(json.tail)
       case Some('\r') => tokenize(json.tail)
       case Some('\n') => tokenize(json.tail)
