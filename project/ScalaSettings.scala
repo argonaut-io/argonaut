c1270e8651eff24154bb2bd41850adc6fd75af99
markhibberd <mark@hibberd.id.au>
2013-03-25 12:16:46 +1100

    Add 2.9.3 cross compile.

--- project/ScalaSettings.scala
+++ project/ScalaSettings.scala
@@ -7,7 +7,7 @@ object ScalaSettings {
 
   lazy val all: Seq[Sett] = Seq(
     scalaVersion := "2.10.0"
-  , crossScalaVersions := Seq("2.9.2", "2.10.0")
+  , crossScalaVersions := Seq("2.9.2", "2.9.3", "2.10.0")
   , scalacOptions <++= onVersionTask(
       all = Seq("-deprecation", "-unchecked", "-optimise")
     , on210 = Seq("-Yinline-warnings", "-feature", "-language:implicitConversions", "-language:higherKinds", "-language:postfixOps")
