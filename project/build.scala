c170be9889a52e4112a3d84ed7a01ef20945121d
markhibberd <mark@hibberd.id.au>
2013-04-08 23:01:22 +1000

    Updated scalaz.

--- project/build.scala
+++ project/build.scala
@@ -11,7 +11,7 @@ object build extends Build {
       organization := "io.argonaut"
   )
 
-  val scalaz = "org.scalaz" %% "scalaz-core" % "7.0.0-M9"
+  val scalaz = "org.scalaz" %% "scalaz-core" % "7.0.0-RC1"
   val scalacheck = "org.scalacheck" %% "scalacheck" % "1.10.0" % "test" cross(CrossVersion.full)
   val specs2_1_12_3 = "org.specs2" %% "specs2" % "1.12.3" % "test"
   val specs2_1_13 = "org.specs2" %% "specs2" % "1.13" % "test"
