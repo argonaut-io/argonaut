14d5323d18fdc27e913ec9d9b45ef8d6651e5405
Sean Parsons <github@futurenotfound.com>
2013-04-21 14:42:11 +0100

    Generate a lot of the DecodeJson/EncodeJson type that vary by arity.

--- src/test/scala/argonaut/CodecSpecification.scala
+++ src/test/scala/argonaut/CodecSpecification.scala
@@ -1,9 +1,12 @@
 package argonaut
 
 import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Prop
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
 import Data._
+import JsonIdentity._
 import org.specs2._, org.specs2.specification._
 import org.specs2.matcher._
 import scalaz._
@@ -14,6 +17,45 @@ object CodecSpecification extends Specification with ScalaCheck {
     prop((a: A) =>
       implicitly[DecodeJson[A]].apply(implicitly[EncodeJson[A]].apply(a).hcursor).value exists (_ === a)
     )
+  
+  case class TestClass(a: Int, b: Int, c: String, d: Int, e: Int, f: String, g: Int, h: Int, i: String, j: Int, k: Int, l: String, m: Int, n: Int, o: String, p: Int, q: Int, r: String, s: Int, t: Int, u: String, v: Boolean)
+
+  implicit val arbTestClass: Arbitrary[TestClass] = Arbitrary(for {
+    a <- arbitrary[Int]
+    b <- arbitrary[Int]
+    c <- arbitrary[String]
+    d <- arbitrary[Int]
+    e <- arbitrary[Int]
+    f <- arbitrary[String]
+    g <- arbitrary[Int]
+    h <- arbitrary[Int]
+    i <- arbitrary[String]
+    j <- arbitrary[Int]
+    k <- arbitrary[Int]
+    l <- arbitrary[String]
+    m <- arbitrary[Int]
+    n <- arbitrary[Int]
+    o <- arbitrary[String]
+    p <- arbitrary[Int]
+    q <- arbitrary[Int]
+    r <- arbitrary[String]
+    s <- arbitrary[Int]
+    t <- arbitrary[Int]
+    u <- arbitrary[String]
+    v <- arbitrary[Boolean]
+  } yield TestClass(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v))
+
+  implicit val equalTestClass: Equal[TestClass] = Equal.equalA
+
+  implicit val testClassEncode: EncodeJson[TestClass] = EncodeJson.jencode22L((x: TestClass) => (x.a, x.b, x.c, x.d, x.e, x.f, x.g, x.h, x.i, x.j, x.k, x.l, x.m, x.n, x.o, x.p, x.q, x.r, x.s, x.t, x.u, x.v))("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v")
+
+  implicit val testClassDecode: DecodeJson[TestClass] = DecodeJson.jdecode22L(TestClass.apply)("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v")
+
+  def encodeL22DecodeL22(): Prop = {
+    prop{testClass: TestClass =>
+      testClass.jencode.jdecode[TestClass].value === testClass.some
+    }
+  }
 
   def is = "Codec" ^
     "Unit encode/decode" ! encodedecode[Unit] ^
