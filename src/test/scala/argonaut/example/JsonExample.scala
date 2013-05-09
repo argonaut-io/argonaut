f8bd08847e968ae575935269381ce2f14d4b028d
markhibberd <mark@hibberd.id.au>
2013-05-10 08:41:55 +1000

    Added example of simple object construction by hand.

---
+++ src/test/scala/argonaut/example/JsonExample.scala
@@ -0,0 +1,36 @@
+package argonaut.example
+
+import argonaut._, Argonaut._
+import scalaz._, Scalaz._
+import org.specs2._
+import org.specs2.specification._
+
+object JsonExample extends Specification {
+  val json =
+    Json.obj(
+      "name" := "fred",
+      "age" := 23,
+      "wallet" := List(
+        Json.obj("value" := 100),
+        Json.obj("value" := 10),
+        Json.obj("value" := 50)
+      )
+    )
+
+  val value =
+    Person("fred", 23, List(Coin(100), Coin(10), Coin(50)))
+
+  case class Coin(value: Int)
+  case class Person(name: String, age: Int, wallet: List[Coin])
+
+  implicit val CodecCoin = casecodec1(Coin.apply, Coin.unapply)("value")
+  implicit val CodecPerson = casecodec3(Person.apply, Person.unapply)("name", "age", "wallet")
+
+  def is = "JsonExample" ^
+    "Can decode hand crafted object" ! {
+      json.as[Person].toOption must beSome(value)
+    } ^
+    "Can encode to match hand crafted object" ! {
+      value.asJson must_== json
+    }
+}
