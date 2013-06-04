46fa764ee6e68799baacd0be2ccb2443529a2b68
markhibberd <mark@hibberd.id.au>
2013-06-04 21:52:17 +1000

    Added spec that tests parse and print for everything in test/resources/data.

---
+++ src/test/scala/argonaut/JsonFilesSpecification.scala
@@ -0,0 +1,32 @@
+package argonaut
+
+import Data._
+import org.scalacheck._
+import org.specs2._, org.specs2.specification._, execute.Result
+import org.specs2.matcher._
+import scalaz._, Scalaz._
+import Argonaut._
+import java.io.File
+
+object JsonFilesSpecification extends Specification with ScalaCheck {
+  def find = new File(getClass.getResource("/data").toURI).listFiles.toList
+
+  case class JsonFile(file: File)
+
+  implicit def JsonFileArbitrary: Arbitrary[JsonFile] =
+    Arbitrary(Gen.oneOf(find.map(JsonFile)))
+
+  def is = "Predefined files" ^
+    "can print and get same result" ! propNoShrink(test) ^
+    end
+
+  val test: JsonFile => Result =
+    jsonfile => {
+      val string = scala.io.Source.fromFile(jsonfile.file).mkString
+      val parsed = string.parseOption
+      val json = parsed.getOrElse(sys.error("could not parse json file [" + jsonfile + "]"))
+      json.nospaces.parseOption must beSome(json)
+      json.spaces2.parseOption must beSome(json)
+      json.spaces4.parseOption must beSome(json)
+    }
+}
