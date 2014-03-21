869b633cc2b004b927a63b8de46396c162e9ee29
xuwei-k <6b656e6a69@gmail.com>
2014-03-21 14:45:36 +0900

    Scala 2.11.0-RC3

--- project/ScalaSettings.scala
+++ project/ScalaSettings.scala
@@ -9,7 +9,7 @@ object ScalaSettings {
 
   lazy val all: Seq[Sett] = Seq(
     scalaVersion := "2.10.3"
-  , crossScalaVersions := Seq("2.9.2", "2.9.3", "2.10.3", "2.11.0-RC1")
+  , crossScalaVersions := Seq("2.9.2", "2.9.3", "2.10.3", "2.11.0-RC3")
   , fork in test := true
   , scalacOptions <++= onVersionTask(
       all = Seq("-deprecation", "-unchecked", "-optimise")
