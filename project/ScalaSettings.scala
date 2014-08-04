766bb020a4099a842d97d8420413b6a301110e50
markhibberd <mark@hibberd.id.au>
2014-08-04 19:26:26 +1000

    release notes and clean-up scala settings.

--- project/ScalaSettings.scala
+++ project/ScalaSettings.scala
@@ -5,16 +5,10 @@ import Tools.onVersionTask
 object ScalaSettings {
   type Sett = Def.Setting[_]
 
-  val on210or211 = Seq("-Yinline-warnings", "-feature", "-language:implicitConversions", "-language:higherKinds", "-language:postfixOps")
-
   lazy val all: Seq[Sett] = Seq(
-    scalaVersion := "2.10.3"
-  , crossScalaVersions := Seq("2.10.3", "2.11.2")
+    scalaVersion := "2.11.2"
+  , crossScalaVersions := Seq("2.10.4", "2.11.2")
   , fork in test := true
-  , scalacOptions <++= onVersionTask(
-      all = Seq("-deprecation", "-unchecked")
-    , on210 = on210or211 :+ "-optimise"
-    , on211 = on210or211 // https://issues.scala-lang.org/browse/SI-8598
-    )
+  , scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature", "-language:_", "-Xlint")
   )
 }
