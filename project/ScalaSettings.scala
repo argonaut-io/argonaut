eaebdab13cfaa0cffd218886e8b6b7d31c06249f
xuwei-k <6b656e6a69@gmail.com>
2017-05-05 15:06:31 +0900

    scala 2.13.0-M1

--- project/ScalaSettings.scala
+++ project/ScalaSettings.scala
@@ -11,12 +11,6 @@ object ScalaSettings {
     Nil
   )
 
-  def sourceDirectoryName(version: String): String =
-    CrossVersion.partialVersion(version) match {
-      case Some((major, minor)) =>
-        s"scala-$major.$minor"
-    }
-
   lazy val all: Seq[Sett] = Seq(
     scalaVersion := "2.11.11"
   , crossScalaVersions := Seq("2.10.6", "2.11.11", "2.12.2")
@@ -25,8 +19,15 @@ object ScalaSettings {
   , scalacOptions ++= PartialFunction.condOpt(CrossVersion.partialVersion(scalaVersion.value)){
       case Some((2, v)) if v >= 11 => unusedWarnings
     }.toList.flatten
-  , unmanagedSourceDirectories in Compile +=
-      (sourceDirectory in Compile).value / sourceDirectoryName(scalaVersion.value)
+  , unmanagedSourceDirectories in Compile ++= {
+      CrossVersion.partialVersion(scalaVersion.value) match {
+        case Some((2, v)) if v >= 11 =>
+          val base = baseDirectory.value.getParentFile / "shared/src/main"
+          Seq(base / "scala-2.11+")
+        case _ =>
+          Nil
+      }
+    }
   ) ++ Seq(Compile, Test).flatMap(c =>
     scalacOptions in (c, console) ~= {_.filterNot(unusedWarnings.toSet)}
   )
