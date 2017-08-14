ca78114cdadfe7c3424db84f04a806e5ec1b5ca9
xuwei-k <6b656e6a69@gmail.com>
2017-08-14 10:03:59 +0900

    scalaz 7.2.15

--- project/build.scala
+++ project/build.scala
@@ -18,7 +18,7 @@ object build {
       organization := "io.argonaut"
   )
 
-  val scalazVersion              = "7.2.14"
+  val scalazVersion              = "7.2.15"
   val paradiseVersion            = "2.1.0"
   val monocleVersion             = "1.4.0"
   val catsVersion                = "0.9.0"
