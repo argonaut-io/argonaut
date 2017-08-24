46f6d5a02e614bbab3ee3647f0b6814788e3b2cc
xuwei-k <6b656e6a69@gmail.com>
2017-08-25 05:32:58 +0900

    enable JsonFilesSpecification in scala.js

---
+++ argonaut/js/src/test/scala/argonaut/JsonFilesSpecification.scala
@@ -0,0 +1,20 @@
+package argonaut
+
+import scalajs.js
+import scalajs.js.Dynamic
+
+object JsonFilesSpecification extends JsonFilesSpecBase {
+  val fs = Dynamic.global.require("fs")
+
+  def listFiles(path: String): Seq[String] =
+    fs.readdirSync(path).asInstanceOf[js.Array[String]]
+
+  override def testData = {
+    listFiles(baseDir).map(path =>
+      TestData(
+        fileName = path.split('/').last,
+        jsonString = fs.readFileSync(baseDir + "/" + path).toString
+      )
+    )
+  }
+}
---
+++ argonaut/shared/src/test/scala/argonaut/JsonFilesSpecBase.scala
@@ -0,0 +1,34 @@
+package argonaut
+
+import argonaut.Argonaut._
+import org.specs2.specification.core.Fragments
+import JsonFilesSpecBase.TestData
+
+object JsonFilesSpecBase {
+  final case class TestData(fileName: String, jsonString: String)
+}
+
+abstract class JsonFilesSpecBase extends ArgonautSpec {
+  def is = s2"""
+  Predefined files can print and get same result  $test
+  """
+
+  val TestData = JsonFilesSpecBase.TestData
+
+  def testData: Seq[TestData]
+
+  def test: Fragments =
+    testData.map(testFile).reduce(_.append(_))
+
+  val baseDir = "./argonaut/jvm/src/test/resources/data"
+
+  def testFile(t: TestData) = {
+    val parsed = t.jsonString.parseOption
+    val json = parsed.getOrElse(sys.error("could not parse json file [" + t.fileName + "]"))
+    s2"""
+    ${t.fileName}
+      With no spaces          ${json.nospaces.parseOption must beSome(json)}
+      With 2 spaces           ${json.spaces2.parseOption must beSome(json)}
+      With 4 spaces           ${json.spaces4.parseOption must beSome(json)}"""
+  }
+}
