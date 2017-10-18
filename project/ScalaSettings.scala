715422bdd704b3018ae3409c68a51f78370a19c3
xuwei-k <6b656e6a69@gmail.com>
2017-10-18 10:04:55 +0900

    Scala 2.12.4

--- project/ScalaSettings.scala
+++ project/ScalaSettings.scala
@@ -15,7 +15,7 @@ object ScalaSettings {
 
   lazy val all: Seq[Sett] = Seq(
     scalaVersion := Scala211
-  , crossScalaVersions := Seq("2.10.6", Scala211, "2.12.3")
+  , crossScalaVersions := Seq("2.10.6", Scala211, "2.12.4")
   , fork in test := true
   , scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature", "-language:_", "-Xlint", "-Xfuture")
   , scalacOptions ++= PartialFunction.condOpt(CrossVersion.partialVersion(scalaVersion.value)){
