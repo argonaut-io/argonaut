0f0164f68d6fde6148a12ca7bccde3c7195076d1
kenji yoshida <6b656e6a69@gmail.com>
2018-11-06 10:02:54 +0900

    scalaz 7.2.27

--- project/build.scala
+++ project/build.scala
@@ -21,7 +21,7 @@ object build {
       organization := "io.argonaut"
   )
 
-  val scalazVersion              = "7.2.26"
+  val scalazVersion              = "7.2.27"
   val paradiseVersion            = "2.1.0"
   val monocleVersion             = "1.5.0"
   val catsVersion                = "1.2.0"
