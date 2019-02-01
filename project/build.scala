1c0ada1bcd61e79e70cc5a72d49cd84acaed35ce
kenji yoshida <6b656e6a69@gmail.com>
2019-02-01 13:40:37 +0900

    Update cats 1.6.0

--- project/build.scala
+++ project/build.scala
@@ -24,7 +24,7 @@ object build {
   val scalazVersion              = "7.2.27"
   val paradiseVersion            = "2.1.0"
   val monocleVersion             = "1.6.0-M1"
-  val catsVersion                = "1.5.0"
+  val catsVersion                = "1.6.0"
 
   val scalacheckVersion          = settingKey[String]("")
   val specs2Version              = settingKey[String]("")
