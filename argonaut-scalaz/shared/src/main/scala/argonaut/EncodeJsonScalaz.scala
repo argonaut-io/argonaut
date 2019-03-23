a31a53c97b52b84bde61861559e28ae013f8a130
kenji yoshida <6b656e6a69@gmail.com>
2019-03-23 13:48:43 +0900

    add Contravariant instances of EncodeJsonNumber, EncodePossibleJsonNumber (#323)

--- argonaut-scalaz/shared/src/main/scala/argonaut/EncodeJsonScalaz.scala
+++ argonaut-scalaz/shared/src/main/scala/argonaut/EncodeJsonScalaz.scala
@@ -48,4 +48,12 @@ trait EncodeJsonScalazs {
   implicit val EncodeJsonContra: Contravariant[EncodeJson] = new Contravariant[EncodeJson] {
     def contramap[A, B](r: EncodeJson[A])(f: B => A) = r contramap f
   }
+
+  implicit val EncodeJsonNumberInstance: Contravariant[EncodeJsonNumber] = new Contravariant[EncodeJsonNumber] {
+    def contramap[A, B](r: EncodeJsonNumber[A])(f: B => A) = r contramap f
+  }
+
+  implicit val EncodePossibleJsonNumberInstance: Contravariant[EncodePossibleJsonNumber] = new Contravariant[EncodePossibleJsonNumber] {
+    def contramap[A, B](r: EncodePossibleJsonNumber[A])(f: B => A) = r contramap f
+  }
 }
