ea9621642cb05c20ca8fdfb4a510b9a520dd0411
xuwei-k <6b656e6a69@gmail.com>
2019-04-26 08:47:18 +0900

    Update cats to 2.0.0-M1

--- project/build.scala
+++ project/build.scala
@@ -28,7 +28,7 @@ object build {
         "1.6.0-M4"
     }
   )
-  val catsVersion                = "1.6.0"
+  val catsVersion                = "2.0.0-M1"
 
   val scalacheckVersion          = settingKey[String]("")
   val specs2Version              = settingKey[String]("")
