23936efa5d1e0ab637f81c6bb9077f57187a8cab
kenji yoshida <6b656e6a69@gmail.com>
2020-08-05 13:14:51 +0900

    specs2 4.10.2

--- project/build.scala
+++ project/build.scala
@@ -63,7 +63,7 @@ object build {
       }
     , releaseTagName := tagName.value
     , libraryDependencies ++= reflect(scalaOrganization.value, scalaVersion.value)
-    , specs2Version := "4.10.1"
+    , specs2Version := "4.10.2"
     , ThisBuild / mimaReportSignatureProblems := true
     /*
     , mimaBinaryIssueFilters ++= {
