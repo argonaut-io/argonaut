9acdf184f8567dc196e0c86f3eb7534fc724fa90
xuwei-k <6b656e6a69@gmail.com>
2021-03-30 16:42:43 +0900

    Scala 3.0.0-RC2

--- project/PublishSettings.scala
+++ project/PublishSettings.scala
@@ -7,7 +7,7 @@ import xerial.sbt.Sonatype.autoImport._
 object PublishSettings {
   type Sett = Def.Setting[_]
 
-  def Scala3 = "3.0.0-RC1"
+  def Scala3 = "3.0.0-RC2"
 
   // TODO monocle
   val scala3projects = Seq(
