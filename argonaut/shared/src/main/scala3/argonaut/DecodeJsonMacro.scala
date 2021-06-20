d89f6d9f04313a0fc2f5370fa1cf43bcdca2ce66
xuwei-k <6b656e6a69@gmail.com>
2021-06-20 18:21:12 +0900

    fix #420

--- argonaut/shared/src/main/scala3/argonaut/DecodeJsonMacro.scala
+++ argonaut/shared/src/main/scala3/argonaut/DecodeJsonMacro.scala
@@ -1,6 +1,8 @@
 package argonaut
 
+import scala.deriving.Mirror
+
 trait DecodeJsonMacro { self: DecodeJson.type =>
-  inline def derive[A]: DecodeJson[A] =
-    internal.Macros.summonDecoder[A]
+  inline def derive[A](using Mirror.ProductOf[A]): DecodeJson[A] =
+    internal.Macros.derivedDecoder[A]
 }
--- argonaut/shared/src/main/scala3/argonaut/EncodeJsonMacro.scala
+++ argonaut/shared/src/main/scala3/argonaut/EncodeJsonMacro.scala
@@ -1,6 +1,8 @@
 package argonaut
 
+import scala.deriving.Mirror
+
 trait EncodeJsonMacro { self: EncodeJson.type =>
-  inline def derive[A]: EncodeJson[A] =
-    internal.Macros.summonEncoder[A]
+  inline def derive[A](using Mirror.ProductOf[A]): EncodeJson[A] =
+    internal.Macros.derivedEncoder[A]
 }
