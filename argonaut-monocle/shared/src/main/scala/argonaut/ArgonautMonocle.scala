cdfb592f9546a9ec56b180da0c5ea5e673e66cfb
kenji yoshida <6b656e6a69@gmail.com>
2021-09-19 08:31:54 +0900

    prepare Scala 3

--- argonaut-monocle/shared/src/main/scala/argonaut/ArgonautMonocle.scala
+++ argonaut-monocle/shared/src/main/scala/argonaut/ArgonautMonocle.scala
@@ -2,14 +2,14 @@ package argonaut
 
 object ArgonautMonocle extends ArgonautMonocles
 
-trait ArgonautMonocles extends
-  ACursorMonocles with
-  CursorHistoryMonocles with
-  CursorOpMonocles with
-  DecodeResultMonocles with
-  HCursorMonocles with
-  JsonMonocles with
-  JsonNumberMonocles with
-  JsonObjectMonocles with
-  JsonPaths with
-  PrettyParamsMonocles
+trait ArgonautMonocles
+  extends ACursorMonocles
+  with CursorHistoryMonocles
+  with CursorOpMonocles
+  with DecodeResultMonocles
+  with HCursorMonocles
+  with JsonMonocles
+  with JsonNumberMonocles
+  with JsonObjectMonocles
+  with JsonPaths
+  with PrettyParamsMonocles
